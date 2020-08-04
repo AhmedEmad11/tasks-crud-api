dc925cf67c22fc757d5a324ec9d447b9e06f4bf5
AhmedEmad11 <ahmeddemadd11@gmail.com>
2020-08-04 18:31:07 +0200

    changed the list view to class based view

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import overview, taskList, taskDetail, taskCreate, taskUpdate, taskDelete
+from .views import overview, TaskList, taskDetail, taskCreate, taskUpdate, taskDelete
 urlpatterns = [
     path('', overview, name='overview'),
-    path('task-list/', taskList, name='task-list'),
+    path('task-list/', TaskList.as_view(), name='task-list'),
     path('task-create/', taskCreate, name='task-create'),
     path('task-detail/<str:pk>/', taskDetail, name='task-detail'),
     path('task-update/<str:pk>/', taskUpdate, name='task-update'),
--- api/views.py
+++ api/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import redirect
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics
 
 from .serializers import TaskSerializer
 from .models import Task
-
 
 
 @api_view(['GET'])
@@ -20,11 +20,9 @@ def overview(request):
     return Response(api_urls)
 
 
-@api_view(['GET'])
-def taskList(request):
-    tasks = Task.objects.all()
-    serializer = TaskSerializer(tasks, many=True)
-    return Response(serializer.data)
+class TaskList(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
 
 @api_view(['GET'])
