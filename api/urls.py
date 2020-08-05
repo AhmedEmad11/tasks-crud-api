e3e1c0207eeaaa432967bd52aff1c56ce9dbd73c
AhmedEmad11 <ahmeddemadd11@gmail.com>
2020-08-05 15:26:13 +0200

    changed all views to class based views and made the API more RESTful

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import overview, TaskList, taskDetail, taskCreate, taskUpdate, taskDelete
+from .views import overview, TaskList, TaskDetail, TaskCreate
+
 urlpatterns = [
     path('', overview, name='overview'),
     path('task-list/', TaskList.as_view(), name='task-list'),
-    path('task-create/', taskCreate, name='task-create'),
-    path('task-detail/<str:pk>/', taskDetail, name='task-detail'),
-    path('task-update/<str:pk>/', taskUpdate, name='task-update'),
-    path('task-delete/<str:pk>/', taskDelete, name='task-delete'),
+    path('task/<str:pk>/', TaskDetail.as_view(), name='task-detail'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import redirect
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import generics
@@ -10,11 +9,11 @@ from .models import Task
 @api_view(['GET'])
 def overview(request):
     api_urls = {
-        'List': '/task-list/',
-        'Detail View': '/task-detail/<str:pk>/',
-        'Create': '/task-create/',
-        'Update': '/task-update/<str:pk>/',
-        'Delete': '/task-delete/<str:pk>/',
+        'GET for list of all tasks': '/task-list/',
+        'GET for detail view of one task': '/task/<str:pk>/',
+        'PUT for updating a task': '/task/<str:pk>/',
+        'DELETE': '/task/<str:pk>/',
+        'POST': '/task-create/',
     }
 
     return Response(api_urls)
@@ -25,37 +24,11 @@ class TaskList(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
 
 
-@api_view(['GET'])
-def taskDetail(request, pk):
-    task = Task.objects.get(id=pk)
-    serializer = TaskSerializer(task, many=False)
-    return Response(serializer.data)
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
 
-@api_view(['POST'])
-def taskCreate(request):
-    serializer = TaskSerializer(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def taskUpdate(request, pk):
-    task = Task.objects.get(id=pk)
-    serializer = TaskSerializer(instance=task, data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-
-@api_view(['DELETE'])
-def taskDelete(request, pk):
-    task = Task.objects.get(id=pk)
-    task.delete()
-
-    return Response("Item Successfully Deleted")
+class TaskCreate(generics.CreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
