812120ae43a1fdbf7079a59d597105c22a0b8d45
AhmedEmad11 <ahmeddemadd11@gmail.com>
2021-05-28 15:49:22 +0200

    add description field to task model

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=120)
-    description = models.CharField(max_length=240, default="")
+    description = models.CharField(max_length=240)
     completed = models.BooleanField(default=False, blank=True, null=True)
 
     def __str__(self):
