0ad51c3a6fe454d99a252f9ebdd511d76e28c71b
AhmedEmad11 <ahmeddemadd11@gmail.com>
2021-05-28 15:16:45 +0200

    make the description not nullable

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=120)
-    description = models.CharField(max_length=240, blank=True, null=True)
+    description = models.CharField(max_length=240, default="")
     completed = models.BooleanField(default=False, blank=True, null=True)
 
     def __str__(self):
